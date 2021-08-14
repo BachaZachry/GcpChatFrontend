6a31e60170466f852a9b2b1eaf14bf06da405580
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-08-14 21:49:35 +0200

    Redux setup

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const LoginPage = () => {
+    return (
+        <div>
+            <div>
+                Text component
+            </div>
+            <div>
+                Google Button component
+            </div>
+        </div>
+    )
+}
