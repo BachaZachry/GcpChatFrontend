dbe9b5f74e9769e2b04893a24dd73f03b094dee3
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2021-08-17 11:07:55 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { LoginPage } from './pages/LoginPage';
+import { HomePage} from './pages/HomePage';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route>
+          <HomePage />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
